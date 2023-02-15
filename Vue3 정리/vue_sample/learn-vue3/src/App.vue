22a69cbca104463dcea64befaceaa2257b80dd36
DESKTOP-ILFBTCN\gunbin <gbcho@ishift.co.kr>
2023-02-16 02:31:12 +0900

    vue3 정리

--- Vue3 정리/vue_sample/learn-vue3/src/App.vue
+++ Vue3 정리/vue_sample/learn-vue3/src/App.vue
@@ -1,48 +1,30 @@
 <template>
-  <div>
-    Vue3 Start
-    <h3>반응형 메시지:</h3>
-    <p>{{ reMessage }} <button v-on:click="remChange">반응버튼</button></p>
-    <h3>일반 메시지:</h3>
-    <p>{{ noMessage }} <button v-on:click="nomChange">일반버튼</button></p>
-    <h3>Options API 메시지:</h3>
-    <p>{{ counter }} <button v-on:click="increment">optionsAPI</button></p>
-  </div>
+    <div>
+        <!-- <button v-on:click="state.count++"> count : {{ state.count }}</button> -->
+        <button v-on:click="count++"> count : {{ count }}</button>
+
+        <button v-on:click="time+='!'"> time : {{ time }}</button>
+
+        <button v-on:click="test++"> time : {{ test }}</button>
+    </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import {ref, reactive, toRef, toRefs, readonly} from 'vue'
 
 export default {
-  setup() {
-    const reMessage = ref("Hello World");
-    const remChange = () => {
-      reMessage.value = reMessage.value + "!";
-    };
+    setup () {
+        const state = reactive({
+            count: 0,
+            title: '좋다',
+            time: '111',
+        });
 
-    let noMessage = "Hello World";
-    let nomChange = () => {
-      noMessage = noMessage + "!";
-    };
+        const {count, title} = toRefs(state);
+        let time = readonly(state.time);
+        time = '2222';
 
-    return {
-      reMessage,
-      noMessage,
-      remChange,
-      nomChange,
-    };
-  },
-  data() {
-    return {
-      counter: "Options API",
-    };
-  },
-  methods: {
-    increment() {
-      this.counter = this.counter + "!";
-    },
-  },
-};
+        return {state, count, time, title, test}
+    }
+}
 </script>
-
-<style lang="scss" scoped></style>
