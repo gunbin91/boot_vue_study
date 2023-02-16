9597e32c3505b5547d205ac31173fdb117148802
gbcho <gbcho@ishift.co.kr>
2023-02-16 18:00:42 +0900

    vue3정리_20230216

---
+++ Vue3 정리/vue_sample/learn-vue3/src/4.class,style-binding/App_classbinding.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="text" :class="{ 'text-red': true, bigText: true }">테스트</div>
+    <div class="text" :class="['text-red', bigClasstest]">테스트</div>
+    <div class="text" :class="[redClasstest, bigClasstest]">테스트</div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const redClasstest = ref("text-red");
+    const bigClasstest = ref("bigText");
+    return { redClasstest, bigClasstest };
+  },
+};
+</script>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+
+.bigText {
+  font-size: 50px;
+}
+</style>
