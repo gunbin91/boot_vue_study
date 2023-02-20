99b0014882eb5f0994509e6b1b10656f754429cb
gbcho <gbcho@ishift.co.kr>
2023-02-20 18:06:38 +0900

    vue3 컴포넌트 정리 시작

--- Vue3 정리/vue_sample/learn-vue3/src/App.vue
+++ Vue3 정리/vue_sample/learn-vue3/src/App.vue
@@ -1,49 +1,24 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <div>
+    <TheNavTest></TheNavTest>
+    <viewTest></viewTest>
+    <AppCard /><app-card />
+  </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
+import TheNavTest from "./components/TheNav.vue";
+import TheView from "./components/TheView.vue";
 
 export default {
-  setup () {
-    const wath_value = ref('');
-
-  watch(wath_value, (newValue, oldValue) => {
-    console.log('newValue: ', newValue);
-    console.log('oldValue: ', oldValue);
-  });
-
-    return {wath_value}
-  }
-}
+  setup() {
+    return {};
+  },
+  components: {
+    TheNavTest,
+    viewTest: TheView,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ Vue3 정리/vue_sample/learn-vue3/src/components/TheView.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="item in viewList" :key="item.index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const viewList = reactive([
+      "An item",
+      "A second item",
+      "A third item",
+      "A fourth item",
+    ]);
+    return { viewList };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
